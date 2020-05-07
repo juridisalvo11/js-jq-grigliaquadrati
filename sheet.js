e66e4aba7866934926290aba7de877f53375b781
Juri Di salvo <Juridisalvo11@gmail.com>
2020-05-07 18:10:10 +0200

    Griglia - javascript da finire

--- sheet.js
+++ sheet.js
@@ -1,13 +1,11 @@
-var array_numeri = [];
+$('.quadrato').each(function() {
+    var numero_casuale = genera_random (0, 10);
+    $(this).html(numero_casuale)
+})
 
 for (var i = 0; i < 25; i++) {
-     var numeri_casuali = genera_random(0, 10)
-     array_numeri.push(numeri_casuali)
-    }
-
-    $('.quadrato').html(numeri_casuali)
-
-    console.log(array_numeri);
+    $('.griglia-2').append('<div class="quadrato"></div>')
+}
 
 function genera_random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
